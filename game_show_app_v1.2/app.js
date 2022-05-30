37b9cbe193566724813a5f7b2635235e786fa291
Irvin Torres <irvin7torres7@gmail.com>
2022-05-30 14:39:25 -0700

    adding variables

---
+++ game_show_app_v1.2/app.js
@@ -0,0 +1,12 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+let missed = 0;                // This initializes the missed counter to 0; This variable will be used to keep count of the number of misses
+
+const startGame = document.querySelector('.btn__reset');
+startGame.addEventListener('click', (e) => {
+    console.log('you did it');
+});
+
+
+
+
