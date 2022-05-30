5702821f9170477b478275ab985f11e0cdc3f173
Irvin Torres <irvin7torres7@gmail.com>
2022-05-30 15:22:23 -0700

    Update app.js
    created a add event listener to remove the overlay
    
    create an array of phrases

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -4,9 +4,18 @@ let missed = 0;                // This initializes the missed counter to 0; This
 
 const startGame = document.querySelector('.btn__reset');
 startGame.addEventListener('click', (e) => {
-    console.log('you did it');
+    const hideOverlay = document.getElementById('overlay');
+    hideOverlay.style.display = 'none';
 });
 
+const phrases = [
+    'It is kind of fun to do the impossible',
+    'It is during our darkest moments that we must focus to see the light',
+    'The journey of a thousand miles begin with one step',
+    'Twent years from now you will be more dissapointed ny the things that you did not do than the ones you did do',
+    'Do not be afraid to give up the good to go for the great',
+];
 
-
-
+function getRandomPhraseAsArray(array) {
+    
+};
