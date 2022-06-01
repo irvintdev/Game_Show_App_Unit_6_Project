24f9925a2f9aaf62dce25cd6735b6c6561b43874
Irvin Torres <irvin7torres7@gmail.com>
2022-05-31 21:24:06 -0700

    Update app.js
    created a function that appends each character in the random phrase to #phrase ul

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -12,7 +12,7 @@ const phrases = [
     'It is kind of fun to do the impossible',
     'It is during our darkest moments that we must focus to see the light',
     'The journey of a thousand miles begins with one step',
-    'Twent years from now you will be more dissapointed by the things that you did not do than the ones you did do',
+    'Twenty years from now you will be more dissapointed by the things that you did not do than the ones you did do',
     'Do not be afraid to give up the good to go for the great',
 ];
 
@@ -23,3 +23,30 @@ function getRandomPhraseAsArray(array) {
     const phraseSplit = randomPhrase.split("");
     return phraseSplit;
 };
+
+// getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay(array) {
+    const chosenPhrase = getRandomPhraseAsArray(array);
+    console.log(chosenPhrase);
+    const phraseLength = chosenPhrase.length;
+    console.log(phraseLength);
+    for ( let i = 0; i < chosenPhrase.length; i++) {
+        if ( chosenPhrase[i] === ' ') {
+            let li = document.createElement('li');
+            li.textContent = chosenPhrase[i];
+            li.className = 'space';
+            console.log(li)
+            phrase.appendChild(li);
+        } else {
+            let li = document.createElement('li');
+            li.textContent = chosenPhrase[i];
+            li.className = 'letter';
+            console.log(li)
+            phrase.appendChild(li);
+        }
+    }
+}
+
+addPhraseToDisplay(phrases);
+
